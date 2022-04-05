01f29288dcb7e019a4330337431f7957b07b4a1b
ArJit0009 <arjitg36@gmaail.com>
2022-04-05 16:04:22 +0530

    initialised tailwindcss and added colors and fontsizes

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+             colors :{
+               "color-9398a2": "#9398A2",
+               "color-0e1d2c": "#0E1D2C",
+               "color-0fc2e9": "#0FC2E9",
+               "color-fafafa": "#FAFAFA",
+               "color-333333": "#333333",
+               "color-585858": "#585858",
+               "color-016dd9": "#016DD9",
+               "color-959595": "#959595",
+               "color-FFFFFF": "#FFFFFF",
+               "color-#0000000F": "#0000000F",
+               "color-5C758E": "#5C758E",
+               "color-E0E0E0": "#E0E0E0",
+               "color-212F3D": "#212F3D",
+               "color-F7FAFE": "#F7FAFE",
+               "color-EAF0F7": "#EAF0F7",
+               "color-D0D0D0": "#D0D0D0",
+               "color-F7F7F7": "#F7F7F7",
+               "color-789CBF": "#789CBF",
+             },
+             fontSize: {
+              fontSize: {
+                xs: "12px",
+                sm: "14px",
+                md: "16px",
+                lg: "18px",
+                xl: "20px",
+               "2xl": "29px",
+               "h1": "22px",
+               "h2": "18px",
+               "h3": "16px",
+              },
+  },
+  plugins: [],
+ }
+}
+}
